a27a956046e7e8000b8080987b09f47b1ab653fc
Ndindairene <irene.ndindaa@gmail.com>
2023-09-29 14:48:19 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return "This Saturday, I want to " + activity + "!";
+}
+
+let mondayWork = function(activity = "go to the office") {
+    return "This Monday, I will " + activity + ".";
+}
+
+function wrapAdjective(emphatic = '*') {
+    return function(adjective) {
+      return `You are ${emphatic}${adjective}${emphatic}!`;
+    };
+  }
